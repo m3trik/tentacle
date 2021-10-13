e14fc3cc7eb033883e3fc8830dc4069738c58641
m3trik <m3trik@live.com>
2021-10-13 08:23:00 -0400

    -

--- append_to_path.py
+++ append_to_path.py
@@ -1,22 +1,22 @@
 import sys, os
 
 
-def appendPaths(app,  exclude=('grepWin_backup', '__pycache__', 'docs', '.vscode', 'blender', 'max', 'maya'), verbose=False):
-	'''Append all relevant directories to the python path for the given parent app.
+def appendPaths(app, verbose=False, exclude=('grepWin_backup', '__pycache__', 'docs', '.vscode', 'blender', 'max', 'maya', '.test')):
+	'''Append all relevant directories to the python path of the given parent app.
 
 	:Parameters:
 		app (str) = The parent app to append paths for. (valid: 'maya', 'blender', 'max')
 		exclude (list) = Exclude directories by name.
 		verbose (bool) = Output the results to the console. (Debug)
 	'''
-	path = (__file__.rstrip(__file__.split('\\')[-1])+'tentacle') #get the path to this module.
+	path = (__file__.rstrip(__file__.split('\\')[-1])+'tentacle') #get the path to this module, and format it to get the path root.
 	sys.path.insert(0, path)
 	if verbose:
 		print (path)
 
-	exclude = [i for i in exclude if i!=app]
+	exclude = [i for i in exclude if i!=app] #exclude all other parent apps.
 
-	#recursively append subdirectories to the system path
+	# recursively append subdirectories to the system path.
 	for root, dirs, files in os.walk(path):
 		for dir_name in dirs:
 
